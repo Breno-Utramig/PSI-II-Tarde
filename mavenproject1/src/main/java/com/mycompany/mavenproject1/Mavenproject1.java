4c4f3559b0279f493cea3e33fdfb818e3aee3419
Breno Rafael Rocha <breno.rocha@serpro.gov.br>
2025-09-02 16:45:11 -0300

    commit inicial

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author S015141186
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Utramig-git");
+    }
+}
